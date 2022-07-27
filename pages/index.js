ee13672fe3ca35fcd00867d1772a93843227fd6d
Pavel <93088269+KhaninPasha0488@users.noreply.github.com>
2022-07-27 15:23:40 +0300

    1

---
+++ pages/index.js
@@ -0,0 +1,18 @@
+import Link from "next/link";
+
+const Home = () => {
+ return(<>
+      <Link href="/users">
+<a>
+ users
+</a>
+      </Link>
+      <h1>Hello world</h1>
+
+     </>
+   
+ )
+};
+
+
+ export default Home;
---
+++ pages/users.js
@@ -0,0 +1,10 @@
+
+
+const Users =()=>{
+    return (
+        <div>
+            <h1>User</h1>
+        </div>
+    )
+}
+export default Users;
